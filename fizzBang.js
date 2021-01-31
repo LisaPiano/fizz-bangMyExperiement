956c19992b4a381c6104ea27f1b0157bb390af9d
LisaPiano <65650875+LisaPiano@users.noreply.github.com>
2021-01-30 22:43:10 -0700

    Create fizzBang.js

---
+++ fizzBang.js
@@ -0,0 +1,11 @@
+
+for (let i = 0; i <= 50; i++){
+    if ((i % 3 === 0) && (i % 5 === 0)){
+        console.log(i + " Fizz-Bang!")
+    } else if(i % 3 === 0){
+    console.log(i + " Fizz!");
+    }
+    else if(i % 5 === 0){
+        console.log(i + " Bang!");
+        }   
+}
